cd7ffbdea9ebd0c4ca6f0e1b35eef2f7510b6aaf
Darek Podczasik <boostertech@mail.com>
2024-07-02 20:33:13 +0100

    initialFriends var created

---
+++ src/initialFriends.js
@@ -0,0 +1,22 @@
+const initialFriends = [
+  {
+    name: "Clark",
+    id: 1,
+    image: "https://i.pravatar.cc/48",
+    balance: 0,
+  },
+  {
+    name: "Sarah",
+    id: 2,
+    image: "https://i.pravatar.cc/48",
+    balance: -10,
+  },
+  {
+    name: "Anthony",
+    id: 3,
+    image: "https://i.pravatar.cc/48",
+    balance: 10,
+  },
+];
+
+export default initialFriends;
