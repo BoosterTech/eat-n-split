6120c0993a8a92f219cf418ffb0135f9d0a5d009
Darek Podczasik <boostertech@mail.com>
2024-07-02 21:41:39 +0100

    <Friend> styles added using Styled-Components

---
+++ src/components/Friend/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const FriendListItem = styled.li`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const ImageContainer = styled.img`
+  border-radius: 100px;
+`;
+
+export const FriendInfo = styled.div``;
+
+export const Name = styled.h3`
+  margin: 0;
+  font-size: 1rem;
+`;
+
+export const Text = styled.p`
+  margin: 0;
+  font-size: 0.9rem;
+`;
+
+export const Button = styled.button`
+  padding: 6px 11px;
+  font-size: 0.8rem;
+  font-weight: 600;
+  border-radius: 5px;
+  border: none;
+  background-color: #F7A75E;
+  margin-left: auto;
+`;
