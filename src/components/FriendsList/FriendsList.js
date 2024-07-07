f9dc57ec04f3c5afb745647c8c47797c9cde1c65
Darek Podczasik <boostertech@mail.com>
2024-07-07 19:49:30 +0100

    * isAddFriendOpen & friends states added * handleOpen & handleAddFriend functions added * condition for displaying <AddFriendForm> added * condition for <Button> added

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import initialFriends from "../../initialFriends";
 import AddFriendForm from "../AddFriendForm/AddFriendForm";
 import Button from "../Button/Button";
 import { Friend } from "../Friend/Friend";
-import { List } from "./styled";
+import { List, Wrapper } from "./styled";
 
 const FriendsList = () => {
-  const friends = initialFriends;
+  const [isAddFriendOpen, setIsAddFriendOpen] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleOpen() {
+    setIsAddFriendOpen(!isAddFriendOpen);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+  }
 
   return (
     <List>
       {friends.map((friend) => (
         <Friend key={friend.id} friend={friend} />
       ))}
-      <AddFriendForm />
-      <Button>Add Friend</Button>
+      {isAddFriendOpen && <AddFriendForm onAddFriend={handleAddFriend} />}
+      <Wrapper>
+        <Button onClick={handleOpen}>
+          {isAddFriendOpen ? "Close" : "AddFriend"}
+        </Button>
+      </Wrapper>
     </List>
   );
 };
