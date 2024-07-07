2eb638a1d4af4a7f013cd26461cd2585df38c5ea
Darek Podczasik <boostertech@mail.com>
2024-07-07 19:44:39 +0100

    <AddFriendForm> created with styled-components

--- src/components/AddFriendForm/AddFriendForm.js
+++ src/components/AddFriendForm/AddFriendForm.js
@@ -1,18 +1,55 @@
-import { Item, Label, Wrapper } from "./styled";
+import { useState } from "react";
+import { Form, FormButton, Item, Label } from "./styled";
 
-function AddFriendForm() {
+function AddFriendForm({ onAddFriend }) {
+  const [friendName, setFriendName] = useState("");
+  const [imageUrl, setImageUrl] = useState("https://i.pravatar.cc/48");
+  const [gender, setGender] = useState("male");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name: friendName,
+      image: `${imageUrl}?gender=${gender}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+    setFriendName("");
+    setImageUrl("https://i.pravatar.cc/48");
+  }
+
   return (
-    <Wrapper>
+    <Form onSubmit={handleSubmit}>
       <Item>
-        <Label for="friendName">👪 Friend name</Label>
-        <input placeholder="" />
+        <Label htmlFor="name">👪 Friend name</Label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder=""
+          value={friendName}
+          onChange={(e) => setFriendName(e.target.value)}
+          autoComplete="name"
+        />
       </Item>
 
       <Item>
-        <Label>🏞️ Image URL</Label>
-        <input value={` https://i.pravatar.cc/48`}/>
+        <Label htmlFor="imageUrl">🏞️ Image URL</Label>
+        <input
+          id="imageUrl"
+          name="imageUrl"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          autoComplete="url"
+        />
       </Item>
-    </Wrapper>
+
+      <FormButton>Add</FormButton>
+    </Form>
   );
 }
 
--- src/components/AddFriendForm/styled.js
+++ src/components/AddFriendForm/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Form = styled.form`
   display: flex;
   flex-direction: column;
   max-width: 340px;
@@ -16,4 +16,20 @@ export const Item = styled.div`
 
 export const Label = styled.label`
   font-weight: bold;
+`;
+
+export const FormButton = styled.button`
+  display: block;
+  padding: 6px 11px;
+  font-size: 0.8rem;
+  font-weight: 600;
+  border-radius: 5px;
+  border: none;
+  background-color: #f7a75e;
+  margin-top: 8px;
+  text-align: center;
+
+  @media (hover: hover) {
+    cursor: pointer;
+  }
 `;
